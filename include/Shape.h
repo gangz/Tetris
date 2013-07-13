1593194785cc45a75add1e08e8ead86f90d560c6
gangz <gangz2009@gmail.com>
2013-07-13 21:54:56 +0800

    display shape in GUI - add forgotten files

---
+++ include/Shape.h
@@ -0,0 +1,17 @@
+/*
+ * Shape.h
+ *
+ *  Created on: Jul 13, 2013
+ *      Author: topcoder
+ */
+
+#ifndef SHAPE_H_
+#define SHAPE_H_
+
+class Shape {
+public:
+	Shape();
+	virtual ~Shape();
+};
+
+#endif /* SHAPE_H_ */
---
+++ src/Shape.cpp
@@ -0,0 +1,18 @@
+/*
+ * Shape.cpp
+ *
+ *  Created on: Jul 13, 2013
+ *      Author: topcoder
+ */
+
+#include "Shape.h"
+
+Shape::Shape() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Shape::~Shape() {
+	// TODO Auto-generated destructor stub
+}
+
