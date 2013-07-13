9a6862e9da3b0a93cea1cd2d06e998f1bf94f0fb
topcoder <topcoder@topcoder-vm.(none)>
2013-07-13 14:23:16 +0800

    modify interface of graphcis driver

--- include/IGraphcisDriver.h
+++ include/IGraphcisDriver.h
@@ -2,11 +2,16 @@
 #define IGRAPHCIS_DRIVER_H_
 
 #include <string>
+
+struct Point{
+	int x;
+	int y;
+};
 class IGraphcisDriver{
 public:
 	virtual ~IGraphcisDriver(){};
-	virtual void drawBox()=0;
-	virtual void drawLine()=0;
-	virtual void writeText(std::string text)=0;
+	virtual void drawBox(Point left_top, Point right_bottom)=0;
+	virtual void drawLine(Point start, Point end)=0;
+	virtual void writeText(Point pos, std::string text)=0;
 };
 #endif /* IGRAPHCIS_DRIVER_H_ */
